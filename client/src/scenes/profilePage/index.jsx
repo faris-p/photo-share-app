94b8dabda94a7d538bfab5eb9b80c94c7cc394ce
faris-p <pfaris510@gmail.com>
2024-03-08 17:06:28 +0530

    profile page completed

--- client/src/scenes/profilePage/index.jsx
+++ client/src/scenes/profilePage/index.jsx
@@ -1,12 +1,56 @@
-import React from 'react';
+import {useState,useEffect} from 'react';
+import {Box,useMediaQuery} from "@mui/material"
+import {useSelector} from "react-redux"
 import { useParams } from 'react-router-dom';
+import Navbar from "../../scenes/navbar"
+import FriendsListWidget from '../widgets/FriendListWidget';
+import MyPostWidgets from '../widgets/MyPostWidgets';
+import PostsWidget from '../widgets/PostsWidget';
+import UserWidget from '../widgets/Userwidget';
 
 function ProfilePage() {
+  const [user,setUser] = useState(null)
   const { userId } = useParams();
+  const token = useSelector(state=>state.token);
+  const isNonMobileScreens = useMediaQuery("(min-width:1000px)")
+  
+  const getUser = async ()=>{
+    const response = await fetch(`http://localhost:3001/users/${userId}`,{
+      method: "GET",
+      headers: {Authorization: `Bearer ${token}`}
+    })
+    const data = await response.json();
+    setUser(data)
+  }
+  useEffect(()=>{
+    getUser();
+  },[])
+
+  if(!user) return null;
   return (
-    <div>
-      ProfilePage {userId}
-    </div>
+    <Box>
+      <Box
+        width="100%" 
+        padding="2rem 6%"
+        display={isNonMobileScreens ? "flex" : "block"}
+        gap="2rem"
+        justifyContent="center"
+        >
+        <Box flexBasis={isNonMobileScreens ? "26%" : undefined}>
+          <UserWidget userId={userId} picturePath={user.picturePath} />
+          <Box m="2rem 0" />
+          <FriendsListWidget userId={userId} />
+        </Box>
+        <Box  flexBasis={isNonMobileScreens ? "42%" : undefined }
+        mt={isNonMobileScreens? undefined : "2rem"}
+        > 
+        <MyPostWidgets picturePath={user.picturePath} /> 
+        <Box m="2rem 0" />
+        <PostsWidget userId={userId} isProfile/> 
+        </Box>
+       
+      </Box>
+    </Box>
   );
 }
 
